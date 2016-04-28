69be573d7db4bab28d0b435ed08b88aa5e927680
Yu Liu <yul.liuyu@gmail.com>
2016-04-27 21:12:24 -0700

    naive bayes miniproject

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,20 +12,38 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/home/yu/MachineLearning/udacity_class_projects/ud120-projects/tools/")
 from email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
 
+time0 = time()
+features_train, features_test, labels_train, labels_test = preprocess()
+print "Preprocessing time:", time()-time0, "s"
 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+
+time0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", time()-time0, "s"
+
+
+# Calculate score. The provided method is
+# clf.score(features_test)
+
+time0 = time()
+pred = clf.predict(features_test)
+print "Prediction time:", time()-time0, "s"
+score = numpy.float(sum(pred == labels_test))/len(labels_test)
 
 
 #########################################################
--- tools/startup.py
+++ tools/startup.py
@@ -25,23 +25,23 @@ try:
 except:
     print "you should install sklearn before continuing"
 
-print
-print "downloading the Enron dataset (this may take a while)"
-print "to check on progress, you can cd up one level, then execute <ls -lthr>"
-print "Enron dataset should be last item on the list, along with its current size"
-print "download will complete at about 423 MB"
-import urllib
-url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tgz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tgz") 
-print "download complete!"
-
-
-print
-print "unzipping Enron dataset (this may take a while)"
-import tarfile
-import os
-os.chdir("..")
-tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
-tfile.extractall(".")
+#print
+#print "downloading the Enron dataset (this may take a while)"
+#print "to check on progress, you can cd up one level, then execute <ls -lthr>"
+#print "Enron dataset should be last item on the list, along with its current size"
+#print "download will complete at about 423 MB"
+#import urllib
+#url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tgz"
+#urllib.urlretrieve(url, filename="../enron_mail_20150507.tgz") 
+#print "download complete!"
+#
+#
+#print
+#print "unzipping Enron dataset (this may take a while)"
+#import tarfile
+#import os
+#os.chdir("..")
+#tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
+#tfile.extractall(".")
 
 print "you're ready to go!"
