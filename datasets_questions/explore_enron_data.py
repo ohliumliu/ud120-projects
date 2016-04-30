b55539c46d93ae3bb76f933078dc1476ce0bfafa
Yu Liu <yul.liuyu@gmail.com>
2016-04-30 15:05:04 -0700

    Finish exploring Entron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,16 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+poi_num = sum([int(enron_data[p]['poi']) for p in enron_data.keys()])
 
+print "James Prentice stock value:", enron_data['PRENTICE JAMES']['total_stock_value']
+
+max([enron_data[p]['total_payments'] for p in ['LAY KENNETH L', 'SKILLING JEFFREY K', 'FASTOW ANDREW S']])
+[p for p in enron_data.keys() if enron_data[p]['total_payments'] == 103559793]
+
+len(enron_data) - sum([int(enron_data[p]['salary']=='NaN') for p in enron_data.keys()])
+len(enron_data) - sum([int(enron_data[p]['email_address']=='NaN') for p in enron_data.keys()])
+
+sum([int(enron_data[p]['total_payments']=='NaN') for p in enron_data.keys()])
+sum([int(enron_data[p]['total_payments']=='NaN') for p in enron_data.keys() if enron_data[p]['poi']])/float(len(enron_data))
+[int(enron_data[p]['total_payments']) for p in enron_data.keys() if enron_data[p]['poi']]
