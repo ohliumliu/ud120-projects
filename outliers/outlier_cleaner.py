820f008a83377fe7d60d0ee1ddcb6e05fb94573f
Yu Liu <yul.liuyu@gmail.com>
2016-04-30 22:47:04 -0700

    enron outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -10,11 +10,12 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
-    
+    error = (predictions - net_worths)**2
+    #uncleaned_data = np.array(zip(ages.ravel(), net_worths.ravel(), error.ravel()))
+    uncleaned_data = np.array([[ages[i][0], net_worths[i][0], error[i][0]] for i in range(0, len(ages))])
+    sorted_data = uncleaned_data[uncleaned_data[:,2].argsort()]
+    cleaned_data_array = sorted_data[0:len(error)-len(error)/10]
+    cleaned_data = [(cleaned_data_array[i][0], cleaned_data_array[i][1], cleaned_data_array[i][2]) for i in range(0, len(cleaned_data_array[:,0]))]
+    ### your code goes here    
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,13 +26,9 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
 
 
@@ -42,6 +38,9 @@ except NameError:
     pass
 plt.scatter(ages, net_worths)
 plt.show()
+
+print "slope:", reg.coef_
+print "score:", reg.score(ages_test, net_worths_test)
 
 
 ### identify and remove the most outlier-y points
@@ -77,7 +76,9 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
-
+    
+    print "slope:", reg.coef_
+    print "score:", reg.score(ages_test, net_worths_test)
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
