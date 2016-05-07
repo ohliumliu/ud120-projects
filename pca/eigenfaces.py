f96338b1952d6e9337038f8fcc6d3044ccb9bfe2
Yu Liu <yul.liuyu@gmail.com>
2016-05-06 23:22:24 -0700

    pca

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -66,7 +66,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -110,6 +110,13 @@ print "done in %0.3fs" % (time() - t0)
 print classification_report(y_test, y_pred, target_names=target_names)
 print confusion_matrix(y_test, y_pred, labels=range(n_classes))
 
+# result: Ariel's f1-score 
+# n-component = 10, f1-score = 0.14
+# n-PC = 15, f1-score = 0.41
+# n-PC = 25, f1-score = 0.58
+# n-PC = 50, f1-score = 0.67
+# n-PC = 100, f1-score = 0.75
+# n-PC = 250, f1-score = 0.64
 
 ###############################################################################
 # Qualitative evaluation of the predictions using matplotlib
